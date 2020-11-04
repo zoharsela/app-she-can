875411a3137a6c4022e5f9206bdccf26c85ac4dc
haleli-amiad <haleli.am@gmail.com>
2020-11-04 10:11:12 +0100

    nav

---
+++ js/cmps/nav.cmp.js
@@ -0,0 +1,9 @@
+export default {
+    template: `
+          <nav>
+                <router-link to="/" exact>Home</router-link>
+                <router-link to="/email" exact>Email</router-link>
+                <router-link to="/keep">Keep</router-link>
+        </nav>
+    `
+}
