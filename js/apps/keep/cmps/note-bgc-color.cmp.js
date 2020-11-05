605f90c4248829060e5c628b6d6c98adfc026624
haleli-amiad <haleli.am@gmail.com>
2020-11-05 10:54:45 +0100

    todo done

--- js/apps/keep/cmps/note-bgc-color.cmp.js
+++ js/apps/keep/cmps/note-bgc-color.cmp.js
@@ -3,24 +3,19 @@
 
 export default {
   props: ['notes'],
-  name: 'notes-list',
+  name: 'notes-bgc',
   template: `<section class="color-setter">
-                 <button class="color-pick pointer" value="#8293e8" style="background-color: #8293e8">.</button>
-                <button class="color-pick pointer" style="background-color: #f0658c">.</button>
-                <button class="color-pick pointer" value="#f07c65" style="background-color: #f07c65">.</button>
-                <button class="color-pick pointer" value="#e3d76b" style="background-color: #e3d76b">.</button>
-                <button class="color-pick pointer" value="#c9e882" style="background-color: #c9e882">.</button>
-                <button class="color-pick pointer" value="#82e8cd" style="background-color: #82e8cd">.</button>
+                <button class="color-pick pointer" @click="emitColor('#8293e8')" style="background-color: #8293e8">.</button>
+                <button class="color-pick pointer" @click="emitColor('#f0658c')" style="background-color: #f0658c">.</button>
+                <button class="color-pick pointer" @click="emitColor('#f07c65')" style="background-color: #f07c65">.</button>
+                <button class="color-pick pointer" @click="emitColor('#e3d76b')" style="background-color: #e3d76b">.</button>
+                <button class="color-pick pointer" @click="emitColor('#c9e882')" style="background-color: #c9e882">.</button>
+                <button class="color-pick pointer" @click="emitColor('#82e8cd')" style="background-color: #82e8cd">.</button>
                 </section>
                 `,
-  data() {
-    return {
-      color: null,
-    }
-  },
   methods: {
-    emitColor() {
-      this.$emit('color', { ...this.color });
+    emitColor(bgcColor) {
+      this.$emit('color', bgcColor);
     },
   }
 }
--- js/services/event-bus.js
+++ js/services/event-bus.js
@@ -4,5 +4,6 @@ export const eventBus = new Vue()
 // Email constim
 
 export const EVENT_DELETE_EMAIL = 'deleteEmail'
+export const EVENT_DO_TODO = 'doTodo'
 // Note constim
 export const EVENT_DELETE_NOTE = 'deleteNote'
