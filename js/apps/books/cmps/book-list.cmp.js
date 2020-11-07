fd918c3e9cc45e85c6c3623615125a926b038c82
haleli-amiad <haleli.am@gmail.com>
2020-11-07 12:34:14 +0100

    books finish

--- js/apps/books/cmps/book-list.cmp.js
+++ js/apps/books/cmps/book-list.cmp.js
@@ -4,7 +4,7 @@ import bookSearchAdd from './book-search-add.cmp.js';
 export default {
     props: ['books'],
     template: `
-        <section class="book-list">
+        <section class="book-list" v-if="books">
             <book-search-add></book-search-add>
             <ul class="book-list-prev" >
                 <li class="book-list-prev" v-for="book in books" :key="book.id">
