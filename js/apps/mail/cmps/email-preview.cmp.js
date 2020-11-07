97aab031e4c026c0fecb88b145eeb219a0f6b9a3
Zohar Sela <zohar10692@gmail.com>
2020-11-07 20:59:21 +0200

    email

--- js/apps/mail/cmps/email-preview.cmp.js
+++ js/apps/mail/cmps/email-preview.cmp.js
@@ -1,9 +1,9 @@
-import { eventBus } from '../../../services/event-bus.js'
-import { emailService } from '../services/email-service.js'
+import { eventBus } from "../../../services/event-bus.js";
+import { emailService } from "../services/email-service.js";
 export default {
-    props: ['email'],
-    name: 'emailPreview',
-    template: `
+  props: ["email"],
+  name: "emailPreview",
+  template: `
             
     <section class="email-preview" @click.stop="goToEmail">
         <div class="email-line flex" :class="{read: email.isRead}" v-if="!emaildeleted">
@@ -21,37 +21,48 @@ export default {
         </div>
     </section>
     `,
-    data() {
-        return {
-            isOpen: false,
-            emailsCategory: this.$route.params.emailsCategory,
-            emaildeleted: false
-        }
+  data() {
+    return {
+      isOpen: false,
+      emailsCategory: this.$route.params.emailsCategory,
+      emaildeleted: false,
+    };
+  },
+  methods: {
+    goToEmail() {
+      this.$router.push("/email/" + this.emailsCategory + "/" + this.email.id);
     },
-    methods: {
-        goToEmail(){
-            this.$router.push('/email/'+this.emailsCategory+'/'+this.email.id)
-        },
-        markedEmail() {
-            emailService.changeToIsRead(this.email.id)
-        },
-        deleteEmail(emailId) {
-            this.emaildeleted = true;
-            emailService.deleteEmail(emailId)
-                .then(() => eventBus.$emit('show-msg', 'Email was successfully Deleted'))
-        },
-        starClicked() {
-            emailService.starEmail(this.email.id)
-                .then(() => eventBus.$emit('show-msg', 'Email was successfully Marked'))
-        },
-        starClickedRemove() {
-            emailService.starEmail(this.email.id)
-                .then(() => eventBus.$emit('show-msg', 'Email was successfully UnMarked'))
-        }
+    markedEmail() {
+      emailService.changeToIsRead(this.email.id);
     },
-    computed: {
-        emailText() {
-            return this.email.body.length > 50 ? this.email.body.substring(0, 49) + '...' : this.email.body;
-        },
-    }
-}
+    deleteEmail(emailId) {
+      this.emaildeleted = true;
+      emailService
+        .deleteEmail(emailId)
+        .then(() =>
+          eventBus.$emit("show-msg", "Email was successfully Deleted")
+        );
+    },
+    starClicked() {
+      emailService
+        .starEmail(this.email.id)
+        .then(() =>
+          eventBus.$emit("show-msg", "Email was successfully Marked")
+        );
+    },
+    starClickedRemove() {
+      emailService
+        .starEmail(this.email.id)
+        .then(() =>
+          eventBus.$emit("show-msg", "Email was successfully UnMarked")
+        );
+    },
+  },
+  computed: {
+    emailText() {
+      return this.email.body.length > 50
+        ? this.email.body.substring(0, 49) + "..."
+        : this.email.body;
+    },
+  },
+};
